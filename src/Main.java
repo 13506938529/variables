544dd7fbf75880eecee15d12c75229054ee765eb
Juan Guzman <guzman.carvajal240406@gmail.com>
2022-10-18 17:44:58 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+public class Main<Scanner> {
+    public void main(String[] args) {
+        Object system = null;
+        Scanner Sc = new Scanner(system.in);
+
+        int dias10;
+        String nombre10;
+        int sueldo10;
+
+        String nombre21;
+        int dias21;
+        int sueldo21;
+
+        int sueldo12;
+        int dias12;
+        String nombre12;
+
+        String nombre40;
+        int dias40;
+        int sueldo40;
+
+        String nombre9;
+        int sueldo9;
+        int dias9;
+
+        sueldo = 3000000;
+        double horas;
+    private static void mostrardatos() {
+        System.out.println("Mr. tamal");
+        System.out.println("Otaku");
+        System.out.println("Robot");
+        System.out.println("Carlos");
+        System.out.println("Africano");
+
+        System.out.println("Buenos dias, tardes o noches trabajador o trabajadora, por favor necesito su edad");
+        dias10 = Sc.nextInt();
+        System.out.println("Buenos dias, tardes o noches trabajador o trabajadora, por favor necesito la cantidad de horas que has hecho");
+        horas10 = Sc.nextByte();
+
+        System.out.println("Su nombre es Mr.tamal, su edad es" +dias10+ " y sus horas de trabajo son" +horas+ " por ultimo su sueldo es "+sueldo10+ " muchas gracias por su cooperacion ");
+        System.out.println("Vamos a Consultar el siguiente");
+
+        System.out.println("Su nombre es Otaku, su edad es" +dias21+ " y sus horas de trabajo son" +horas+ " por ultimo su sueldo es "+sueldo21+ " muchas gracias por su cooperacion ");
+        System.out.println("Vamos a Consultar el siguiente");
+
+        System.out.println("Su nombre es Robot, su edad es" +dias12+ " y sus horas de trabajo son" +horas+ " por ultimo su sueldo es "+sueldo12+ " muchas gracias por su cooperacion ");
+        System.out.println("Vamos a Consultar el siguiente");
+
+        System.out.println("Su nombre es Carlos, su edad es" +dias40+ " y sus horas de trabajo son" +horas+ " por ultimo su sueldo es "+sueldo40+ " muchas gracias por su cooperacion ");
+        System.out.println("Vamos a Consultar el siguiente");
+
+        System.out.println("Su nombre es Africano, su edad es" +dias9+ " y sus horas de trabajo son" +horas+ " por ultimo su sueldo es "+sueldo9+ " muchas gracias por su cooperacion ");
+        System.out.println("Vamos a Consultar el siguiente");
+        
+    }
+
+
+}
